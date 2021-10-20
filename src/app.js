70bd0afb7b780510fc91172ed158492c412042ad
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-10-20 09:54:50 -0400

    Add API data & replace city, temp, current weather

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+}
+
 let apiKey = "fb57d65c8433d702c30132cfdf5708ba";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
