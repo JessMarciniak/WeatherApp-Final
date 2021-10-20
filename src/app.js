acd89fbed3eb2b3ab6ce03c1eb90d16109f7d260
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2021-10-20 10:17:38 -0400

    Swapped Sunrise/set for High/low temp

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@ function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let description = document.querySelector("#description");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#windSpeed");
+  let feelsLikeElement = document.querySelector("#feelsLike");
+  let highTempElement = document.querySelector("#highTemp");
+  let lowTempElement = document.querySelector("#lowTemp");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-  description.innerHTML = response.data.weather[0].description;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windSpeedElement.innerHTML = response.data.wind.speed;
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  highTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  lowTempElement.innerHTML = Math.round(response.data.main.temp_min);
 }
 
 let apiKey = "fb57d65c8433d702c30132cfdf5708ba";
